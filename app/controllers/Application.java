d293cff5dfb685e9f0be514228d7bee4ca3383b1
Juan Tejeria <jt.tejeria@gmail.com>
2013-06-08 18:10:40 -0300

    Register acc, and change password added
    Still missing to implement them though UserController

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,8 +10,13 @@ import views.html.*;
 import static play.data.Form.*;
 
 public class Application extends Controller {
-  @EJB(beanName = "UserController")
+    @EJB(beanName = "UserController")
     UserController userController;      //cambiar por metodo getBean???
+    private static final String EMAIL_ERROR ="Email already used, please try another";
+    private static final String PASSWORD_MISSMATCH ="The passwords you inserted did not match";
+    private static final String SUCCEDED =" created. Please head back to login page and sign in !";
+    private static final String SUCCEDED_TO_CHANGE_PASSWORD ="You have updated your password succesfully";
+    static  String token=""; //poner constante con invalid
 
     /**
      * Describes the login form.
@@ -21,8 +26,30 @@ public class Application extends Controller {
         public String password;
     }
 
+    /**
+     * Describes the Register form.
+     */
+    public static class Register {
+        public String email;
+        public String name;
+        public String password;
+        public String secondPassword;
+    }
+    public static class PasswordChanger{
+        public String oldPassword;
+        public String newPassword;
+        public String newPasswordConfirm;
+    }
+
     public static Result index() {
         return ok(index.render(form(Login.class)));
+    }
+
+    public static Result registerForm(){
+        return ok(registerIndex.render(form(Register.class)));
+    }
+    public static Result changePassword(){
+        return ok(changePasswordIndex.render(form(PasswordChanger.class)));
     }
 
     public static Result login(){
@@ -41,5 +68,42 @@ public class Application extends Controller {
     public static Result register(){
         //something
         return ok(homePage.render("Register"));
+    }
+
+    public static Result registering(){
+        String message;
+        Form<Register> form= form(Register.class).bindFromRequest();
+        Register information = form.get();
+
+        //intentar persistir informacion. Debe ir en User Controller
+        if(information.password.equals(information.secondPassword)){
+            try {
+                userController.register(information.name,information.email,information.password);
+                message="Account "+information.email+" created. Please go back and login to start watching!";
+                return ok(register.render(message));
+            } catch (CancelActionException e) {
+                return ok(registerIndex.render(form(Register.class)));
+            }
+        }else{
+            message=PASSWORD_MISSMATCH;
+            return ok(registerIndex.render(form(Register.class)));
+        }
+    }
+
+    public static Result changeMyPassword(){
+        String message;
+        Form<PasswordChanger> form = form(PasswordChanger.class).bindFromRequest();
+        PasswordChanger information = form.get();
+        if(information.newPassword.equals(information.newPasswordConfirm)){
+            if(userController.updatePassword(information.oldPassword,information.newPassword)){
+                message=SUCCEDED_TO_CHANGE_PASSWORD;
+                return ok(changePassword.render(message));
+            } else{
+                return ok(changePasswordIndex.render(form(PasswordChanger.class)));
+            }
+        }else{
+            return ok(changePasswordIndex.render(form(PasswordChanger.class)));
+        }
+
     }
 }
