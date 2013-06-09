810dbaa8378e58f26b8ba57da6035aabed95ee48
mmanasliski <mmanasliski@correo.um.edu.uy>
2013-06-08 21:43:46 -0300

    test packages created

---
+++ test/ControllersTests/UserControllerTest.java
@@ -0,0 +1,18 @@
+package ControllersTests;
+
+import java.util.ResourceBundle;
+
+public class UserControllerTest {
+
+    // Configuration file name
+    private static final String PROPERTIES = "conf.dao_factory";
+    private static ResourceBundle rb = ResourceBundle.getBundle(PROPERTIES);
+
+    // Key for the name of the classes that implement MovieDao and AuthenticationHandler
+    private static final String AUTH_HANDLER_IMPL_K ="AUTH_HANDLER_IMPL";
+    //agregar las key de user manager y las necesarias de model
+
+
+
+
+}
