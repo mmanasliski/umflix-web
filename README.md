bad721fa7297ea5ad929b2c56e3dff0c57d30cae
Juan Tejeria <jt.tejeria@gmail.com>
2013-06-13 20:56:00 -0300

    Deleted old README, and modified new

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Webapp module for UMFlix
+
+Web application for UMFlix. Includes website with some basic features as login, creating account, changing password, search and watch movies.
+
 ==========
 
 
@@ -17,6 +20,16 @@ How to run Webapp module.
                                  <version>1.0-SNAPSHOT</version>
                              </dependency>
                         mvn install current project
- 2) Install playFramework http://www.playframework.com/download
+ 2) Please download and "mvn install" the following repositories from which umflix depend:
+        https://github.com/haretche2/clipstorage
+        https://github.com/marshhxx/modelstorage
+        https://github.com/scarrera/movie-manager
+        https://github.com/martinbomio/usermanager
+        https://github.com/haretche2/autenticationhandler
+        https://github.com/mmanasliski/catalog-service
+ 3) Install playFramework http://www.playframework.com/download
     cd directory of umflix-web
-    play run
+    mvn play2:compile
+    mvn play2:copy-dependencies
+    mvn play2:run
+ 4) If the movie is not shown and ur not using Mozilla Firefox, install(or open with) Mozilla Firefox browser in order to see movies.
--- test/ControllersTests/UserControllerTest.java
+++ test/ControllersTests/UserControllerTest.java
@@ -1,10 +1,13 @@
 package ControllersTests;
 
 import java.util.ResourceBundle;
+
+import controllers.MoviePlayerController;
 import controllers.UserController;
 import edu.umflix.authenticationhandler.AuthenticationHandler;
 import edu.umflix.authenticationhandler.exceptions.InvalidUserException;
 import edu.umflix.authenticationhandler.impl.AuthenticationHandlerImpl;
+import edu.umflix.model.Role;
 import edu.umflix.model.User;
 import edu.umflix.usermanager.UserManager;
 import edu.umflix.usermanager.exceptions.EmailAlreadyTakenException;
@@ -17,6 +20,7 @@ import org.mockito.Mockito;
 import edu.umflix.authenticationhandler.impl.AuthenticationHandlerImpl;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
 public class UserControllerTest {
     /*
@@ -24,27 +28,29 @@ public class UserControllerTest {
 +     * This class mocks UserController so it uses mocked classes for the UserManager's methods implementation
 +     *
 +     */
+
+
     class UserControllerMock extends UserController {
         private AuthenticationHandler authHandler = Mockito.mock(AuthenticationHandlerImpl.class);
-        private UserManager userManager = Mockito.mock(UserManager.class);     //Cambiar por la implementacion de UserManager
-
+        private UserManager userManager = Mockito.mock(UserManager.class);
     }
 
     @Test
     public void testLogin(){
+        Role roleUser = new Role((long)0);
         UserControllerMock testedMock = new UserControllerMock();
-        String userEmail="";
-        String password="";
-        User user = new User(userEmail, null, password, null);
+        String userEmail="hola@gmail.com";
+        String password="password";
+        User user = new User(userEmail, null, password, roleUser);
         try {
             testedMock.login(userEmail,password);
         } catch (CancelActionException e) {
-            fail("Error al login");
+            fail();
         }
         try {
             Mockito.verify(testedMock.userManager).login(user);
         } catch (InvalidUserException e) {
-            fail("User invalido");
+            fail();
         }
 
     }
@@ -59,18 +65,18 @@ public class UserControllerTest {
         try {
             testedMock.register(userEmail,name,password);
         } catch (CancelActionException e) {
-            fail("Error en registro");
+            fail();
         }
         try {
             Mockito.verify(testedMock.userManager).register(user);
         } catch (InvalidEmailException e) {
-            fail("Invalid email");
+            fail();
         } catch (InvalidPasswordException e) {
-            fail("Invalid password");
+            fail();
         } catch (InvalidRoleException e) {
-            fail("Invalid role");
+            fail();
         } catch (EmailAlreadyTakenException e) {
-            fail("Email taken");
+            fail();
         }
     }
 }
