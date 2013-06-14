198eb6189070e575aff6669bd9865e53d0cee875
mmanasliski <mmanasliski@correo.um.edu.uy>
2013-06-14 19:48:41 -0300

    Mockito dependencies and MoviePlayerControllerTest

---
+++ test/ControllersTests/MoviePlayerControllerTest.java
@@ -0,0 +1,11 @@
+package ControllersTests;
+
+import exception.CancelActionException;
+
+public class MoviePlayerControllerTest {
+
+
+    protected void startMovie(Long movieID, String userToken) throws CancelActionException {
+
+    }
+}
