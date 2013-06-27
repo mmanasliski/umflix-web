86e117cca617ffbb7425b4eeb0a096487af04a7f
Ivana Calleros <icalleros@correo.um.edu.uy>
2013-06-26 21:17:11 -0300

    Clases a implementar en Bean Factory modificado

--- app/controllers/BeanFactory.java
+++ app/controllers/BeanFactory.java
@@ -1,19 +1,27 @@
 package controllers;
 
 import edu.um.arq.umflix.catalogservice.CatalogService;
+import edu.um.arq.umflix.catalogservice.exception.DaoException;
 import edu.umflix.authenticationhandler.AuthenticationHandler;
+import edu.umflix.authenticationhandler.exceptions.InvalidTokenException;
+import edu.umflix.authenticationhandler.exceptions.InvalidUserException;
+import edu.umflix.exceptions.MovieNotFoundException;
+import edu.umflix.model.*;
 import edu.umflix.usermanager.UserManager;
+import edu.umflix.usermanager.exceptions.*;
 import model.MovieManager;
+import model.exceptions.NoAdsException;
+import model.exceptions.UserNotAllowedException;
+import model.exceptions.ValuesInActivityException;
 
 import javax.naming.InitialContext;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Properties;
 
 /**
- * Created with IntelliJ IDEA.
- * User: telematica
- * Date: 14/06/13
- * Time: 13:26
- * To change this template use File | Settings | File Templates.
+ *
+ * Bean Factory
  */
 public class BeanFactory {
 
@@ -31,16 +39,82 @@ public class BeanFactory {
             p.put("java.naming.provider.url", "ejbd://localhost:4201");
             InitialContext ctx = new InitialContext(p);
             if(key.equals(MOVIE_MANAGER)){
-                o = (MovieManager) ctx.lookup(MOVIE_MANAGER);
+                o = new MovieManager() {
+                    @Override
+                    public List<Clip> getMovie(String s, Long aLong) throws InvalidTokenException, MovieNotFoundException, UserNotAllowedException {
+                        Clip c = new Clip(1L,1);
+                        return null;
+                    }
+
+                    @Override
+                    public ClipData getClipData(String s, Long aLong) throws InvalidTokenException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public void sendActivity(String s, Activity activity) throws InvalidTokenException, ValuesInActivityException, UserNotAllowedException {
+                        //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public ClipData getAd(String s, Long aLong) throws InvalidTokenException, NoAdsException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+                };
             }
             if(key.equals(USER_MANAGER)){
-                o = (UserManager) ctx.lookup(USER_MANAGER);
+                o = new UserManager() {
+                    @Override
+                    public void register(User user) throws InvalidEmailException, InvalidPasswordException, InvalidRoleException, EmailAlreadyTakenException {
+                        //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public String login(User user) throws InvalidUserException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public void delete(String s, User user) throws InvalidUserException, PermissionDeniedException, InvalidTokenException {
+                        //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public void update(String s, User user, String s2) throws InvalidUserException, PermissionDeniedException, InvalidPasswordException, InvalidTokenException {
+                        //To change body of implemented methods use File | Settings | File Templates.
+                    }
+                }             ;
             }
             if(key.equals(AUTH_HANDLER)){
-                o = (AuthenticationHandler)ctx.lookup(AUTH_HANDLER);
+                o = new AuthenticationHandler() {
+                    @Override
+                    public boolean validateToken(String s) {
+                        return false;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public String authenticate(User user) throws InvalidUserException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public User getUserOfToken(String s) throws InvalidTokenException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+
+                    @Override
+                    public boolean isUserInRole(String s, Role role) throws InvalidTokenException {
+                        return false;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+                }    ;
             }
             if(key.equals(CATALOG_SERVICE)){
-                o = (CatalogService)ctx.lookup(CATALOG_SERVICE);
+                o = new CatalogService() {
+                    @Override
+                    public List<Movie> search(String s, String s2) throws DaoException, InvalidTokenException {
+                        return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+                };
             }
             return o;
         } catch (Exception e) {
